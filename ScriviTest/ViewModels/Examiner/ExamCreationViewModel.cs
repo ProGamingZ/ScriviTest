7716553fe1baebc1f8afe6f27d81bce96fd53b8f
YJAO <ortizyuri254@gmail.com>
2026-04-22 14:18:27 +0800

    added using ScriviTest.Services;

--- ScriviTest/ViewModels/Examiner/ExamCreationViewModel.cs
+++ ScriviTest/ViewModels/Examiner/ExamCreationViewModel.cs
@@ -6,6 +6,7 @@ using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ScriviTest.Services;
 namespace ScriviTest.ViewModels.Examiner;
 
 public partial class ExamCreationViewModel : ViewModelBase
